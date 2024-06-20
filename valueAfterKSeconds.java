65d81fcb8cdab4192b96cd6414327162e52f839e
TiasPortal <tiacompsci22@gmail.com>
2024-06-20 11:05:09 +1000

    Create valueAfterKSeconds.java

---
+++ valueAfterKSeconds.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int valueAfterKSeconds(int n, int k) {
+        
+         final int MOD = 1000000007;
+
+        int a[] = new int [n];
+        int newA[] = new int [n];
+
+        for(int i = 0 ; i< n; i++){
+            a[i] = 1;
+           newA[i] = 1;
+        }
+
+       
+
+        for(int time = 0 ; time < k; time++){
+           
+                for(int j = 0; j < n; j++){
+
+                    newA[j] = a[j];
+                    if(j>0){
+                        newA[j] = (newA[j]+newA[j-1])%MOD;
+                    }
+
+                }
+
+
+                for(int i = 0; i < n; i++){
+                    a[i] = newA[i];
+                }
+
+            }
+     
+        
+
+  
+        return a[n-1];
+    }
+}
